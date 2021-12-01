57e5bf4004d7387d253e086ceb2600562edf3fe5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-01 07:51:21 +0200

    day1: parse input

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -1,7 +1,19 @@
 (ns day01.core
   (:gen-class))
 
+; --------------------------
+; common
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n and converts it into a vector of numbers."
+  [s]
+  (->> s
+       clojure.string/split-lines
+       (mapv #(Integer/parseInt %))))
+
+(def depths (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println depths))
