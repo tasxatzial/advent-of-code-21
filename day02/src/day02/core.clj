7f1c0115656b63e4715f6bc46cf24811574b12d1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-02 07:15:43 +0200

    day2: parse input

--- day02/src/day02/core.clj
+++ day02/src/day02/core.clj
@@ -1,7 +1,22 @@
 (ns day02.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n and converts it to the appropriate structure,
+  a vector containing vectors, each one representing an instruction."
+  [s]
+  (->> s
+       clojure.string/split-lines
+       (mapv #(clojure.string/split % #" "))
+       (mapv #(vector (keyword (first %)) (Integer/parseInt (second %))))))
+
+(def instructions (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println instructions))
