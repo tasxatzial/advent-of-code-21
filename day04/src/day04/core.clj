7d3aabc62a1b7e20b83cea724fb22af2dd08e545
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-05 18:50:44 +0200

    day4: parse the first line of input file

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -1,7 +1,18 @@
 (ns day04.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-draw-numbers
+  "Parses the string that represents the numbers drawn and returns
+  a vector of numbers."
+  [s]
+  (let [draw-numbers (clojure.string/split s #",")]
+    (mapv #(Integer/parseInt %) draw-numbers)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (parse-draw-numbers "23,91,18,32,73,14")))
