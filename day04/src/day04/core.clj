3822c5bb60f9f8844fb7c62e8e31a9f64ca68892
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-05 19:17:50 +0200

    day4: parse input and create structures for the drawn numbers and bingo boards

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -27,6 +27,34 @@
                   (mapv #(set %)))]
     (into rows cols)))
 
+(defn parse-boards
+  "Parses a collection of lines (strings) and returns an appropriate structure that represents
+  all bingo boards. The lines are all the lines in the input file except the first one.
+  Returns a vector containing vectors. Each vector is the return value of create-board."
+  [coll]
+  (->> coll
+       (filter seq)
+       (map #(clojure.string/split % #" "))
+       (map #(filter seq %))
+       flatten
+       (map #(Integer/parseInt %))
+       (partition 25)
+       (mapv create-board)))
+
+(defn parse
+  "Splits the input string by newline and creates the structures that represent the drawn
+  numbers and the bingo boards. Returns a vector of two items. First one is the draw-numbers
+  as returned by parse-draw-numbers. Second one is the bingo boards as returned by parse-boards."
+  [s]
+  (let [split-s (clojure.string/split-lines s)
+        draw-numbers (parse-draw-numbers (first split-s))
+        boards (parse-boards (rest split-s))]
+    [draw-numbers boards]))
+
+(def starting-state (parse (slurp input-file)))
+(def draw-numbers (first starting-state))
+(def boards (second starting-state))
+
 (defn -main
   []
-  (println (create-board [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25])))
+  (println draw-numbers))
