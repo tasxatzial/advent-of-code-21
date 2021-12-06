b0710c5c611e0c6cc52d3573a583934629608f9d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-06 02:07:41 +0200

    day5: parse input

--- day05/src/day05/core.clj
+++ day05/src/day05/core.clj
@@ -1,7 +1,30 @@
 (ns day05.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-line
+  "Parses a line (string) and returns a collection of 2 items, each item represents
+  a (x,y) coordinate."
+  [s]
+  (let [split-line (clojure.string/split s #" -> |,")]
+    (->> split-line
+         (map #(Integer/parseInt %))
+         (partition 2))))
+
+(defn parse
+  "Parses the input string and returns a collection of line segments. Each segment
+  is represented by the result of parse-line."
+  [s]
+  (->> s
+       clojure.string/split-lines
+       (map parse-line)))
+
+(def vent-lines (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println vent-lines))
