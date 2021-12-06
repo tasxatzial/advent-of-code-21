57cae05df8812b9c070c66f06d38142a0efc8614
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-06 14:17:45 +0200

    day06: parse input

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -1,7 +1,20 @@
 (ns day06.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Parses the input file and returns collection of numbers."
+  [s]
+  (let [split-lines (clojure.string/split-lines s)
+        ages (first split-lines)]
+    (mapv #(Integer/parseInt %) (clojure.string/split ages #","))))
+
+(def ages (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println ages))
