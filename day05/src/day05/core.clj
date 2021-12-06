1ad3be6967e0326be3e19ebc127288b7ad4f167d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-06 02:35:40 +0200

    day5 problem 1: collect only horizontal or vertical lines

--- day05/src/day05/core.clj
+++ day05/src/day05/core.clj
@@ -25,6 +25,24 @@
 
 (def vent-lines (parse (slurp input-file)))
 
+; --------------------------
+; problem 1
+
+(defn horizontal?
+  "Returns true if the given segment is horizontal, false otherwise."
+  [[[x1 y1] [x2 y2]]]
+  (= y1 y2))
+
+(defn vertical?
+  "Returns true if the given segment is vertical, false otherwise."
+  [[[x1 y1] [x2 y2]]]
+  (= x1 x2))
+
+(defn collect-horizontal-vertical-lines
+  "Collects all horizontal or vertical lines."
+  []
+  (filter #(or (horizontal? %) (vertical? %)) vent-lines))
+
 (defn -main
   []
-  (println vent-lines))
+  (println (collect-horizontal-vertical-lines)))
