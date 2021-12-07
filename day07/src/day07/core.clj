85c442eb0c5d164a97f74de2ce15210eaade80e9
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-07 19:21:25 +0200

    day7: find the total fuel when all crabs move to the position which minimizes fuel consumption

--- day07/src/day07/core.clj
+++ day07/src/day07/core.clj
@@ -15,6 +15,24 @@
 
 (def positions (parse (slurp input-file)))
 
+(defn total-fuel
+  "Finds the total fuel when each crab moves to position x."
+  [positions x fn_fuel]
+  (->> positions
+       (map #(fn_fuel % x))
+       (apply +)))
+
+(defn min-fuel
+  "Finds the total fuel required when every crab moves to the position that
+  minimizes the total fuel consumption."
+  [positions fn_min-fuel-pos fn_fuel]
+  (let [min-pos (fn_min-fuel-pos positions)]
+    (if (int? min-pos)
+      (total-fuel positions min-pos fn_fuel)
+      (let [round-down (Math/round (Math/floor min-pos))]
+        (min (total-fuel positions round-down fn_fuel)
+             (total-fuel positions (inc round-down) fn_fuel))))))
+
 (defn -main
   []
   (println positions))
