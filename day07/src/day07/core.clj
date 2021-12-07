2d72977c85ed719a4c454a95b38f682dd29b4322
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-07 16:19:19 +0200

    day7: parse input

--- day07/src/day07/core.clj
+++ day07/src/day07/core.clj
@@ -1,7 +1,20 @@
 (ns day07.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Parses the input string and returns collection of numbers."
+  [s]
+  (let [split-lines (clojure.string/split-lines s)
+        positions (first split-lines)]
+    (map #(Integer/parseInt %) (clojure.string/split positions #","))))
+
+(def positions (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println positions))
