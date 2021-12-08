487f864c63c09c091ff1101c2c4db186382ec58f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-08 19:54:09 +0200

    day8 problem 1: compute solution

--- day08/src/day08/core.clj
+++ day08/src/day08/core.clj
@@ -22,6 +22,41 @@
 
 (def signals (parse (slurp input-file)))
 
+(def digit->segment-count
+  {0 6
+   1 2
+   2 5
+   3 5
+   4 4
+   5 5
+   6 6
+   7 3
+   8 7
+   9 6})
+
+; --------------------------
+; problem 1
+
+(def output-values (map second signals))
+
+(defn count-digits-with-unique-segments
+  "Takes as input a collection of 4 digits and counts how many times 1 4 7 8 appear in total."
+  [output-value]
+  (let [unique-segments (set (map digit->segment-count [1 4 7 8]))]
+    (count (filter unique-segments (map count output-value)))))
+
+(defn total-digits-with-unique-segments
+  "Finds how many times digits 1 4 7 8 appear in the output values."
+  []
+  (apply + (map count-digits-with-unique-segments output-values)))
+
+; --------------------------
+; results
+
+(defn day08-1
+  []
+  (total-digits-with-unique-segments))
+
 (defn -main
   []
-  (println signals))
+  (println (day08-1)))
