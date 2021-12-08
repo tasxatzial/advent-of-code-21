57a97bdd158d0c46ce1772926355b678ac919700
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-08 18:54:39 +0200

    day8: parse input

--- day08/src/day08/core.clj
+++ day08/src/day08/core.clj
@@ -1,7 +1,27 @@
 (ns day08.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-line
+  "Parses an input line and returns a collection of two items. The first item is
+  a vector of the signal patterns. The second item is a vector of the output values."
+  [s]
+  (let [split-line (clojure.string/split s #" [|] ")]
+    (map #(clojure.string/split % #" ") split-line)))
+
+(defn parse
+  "Parses the input string into a collection that contains the results
+  of parse-line when applied to each input line."
+  [s]
+  (let [split-input (clojure.string/split-lines s)]
+    (map parse-line split-input)))
+
+(def signals (parse (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println signals))
