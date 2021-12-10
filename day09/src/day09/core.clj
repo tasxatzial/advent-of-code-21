43ccb7f3de56431e010bac4fa11e83435a13e40a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-10 23:56:06 +0200

    day9: parse input

--- day09/src/day09/core.clj
+++ day09/src/day09/core.clj
@@ -1,7 +1,34 @@
 (ns day09.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-line
+  "Parses an input line and returns a vector of numbers."
+  [s]
+  (mapv #(- (int %) 48) s))
+
+(defn parse
+  "Parses the input string and returns a collection of vectors of numbers."
+  [s]
+  (let [split-lines (clojure.string/split-lines s)
+        rows (count split-lines)
+        columns (count (first split-lines))
+        heightmap (vec (flatten (map parse-line split-lines)))]
+    [rows columns heightmap]))
+
+(def parsed-input (parse (slurp input-file)))
+
+; a vector of values, stored by rows
+(def heightmap (get parsed-input 2))
+
+(def rows (get parsed-input 0))
+(def columns (get parsed-input 1))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println heightmap))
