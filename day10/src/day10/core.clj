b08f6e07f6e5e4f20788ab0a041de0cd55c453fa
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-11 01:16:49 +0200

    day10: parse input

--- day10/src/day10/core.clj
+++ day10/src/day10/core.clj
@@ -1,7 +1,25 @@
 (ns day10.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Parses the input string and returns a collection of strings (chunks)."
+  [s]
+  (let [split-lines (clojure.string/split-lines s)]
+    split-lines))
+
+(def chunks (parse (slurp input-file)))
+
+(def open-brackets #{\{ \( \[ \<})
+(def close-brackets #{\} \) \] \>})
+
+(def matching-bracket
+  (zipmap open-brackets close-brackets))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println chunks))
