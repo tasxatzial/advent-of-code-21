dec19586246c50db3a0b89307d9141a5aec0898c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-11 03:01:55 +0200

    day10: refactoring & add comments

--- day10/src/day10/core.clj
+++ day10/src/day10/core.clj
@@ -14,39 +14,43 @@
 
 (def chunks (parse (slurp input-file)))
 
-(def open-brackets #{\{ \( \[ \<})
-(def close-brackets #{\} \) \] \>})
+(def open-chars #{\{ \( \[ \<})
+(def close-chars #{\} \) \] \>})
 
-(def matching-bracket
-  (zipmap open-brackets close-brackets))
+(def matching-char
+  (zipmap open-chars close-chars))
 
 (defn open-bracket?
-  "Returns true if c is an opening bracket, false otherwise."
+  "Returns true if c is an opening char, false otherwise."
   [c]
-  (contains? matching-bracket c))
+  (contains? matching-char c))
 
 (defn complete-chunk
   "Finds the missing chunk of an incomplete chunk."
-  [unclosed-brackets]
-  (reverse (map matching-bracket unclosed-brackets)))
+  [unclosed-chars]
+  (reverse (map matching-char unclosed-chars)))
 
 (defn process-chunk
-  "Returns true if the given string has properly matched brackets, false otherwise."
+  "Parses a chunk and returns:
+  1) [:corrupted illegal-char] if the chunk is corrupted.
+  2) [:valid []] if the chunk is valid.
+  3) [:incomplete unclosed-chars] if the chunk is incomplete."
   [s]
   (loop [stack []
-         [bracket & rest-brackets] s]
-    (if bracket
-      (if (open-bracket? bracket)
-        (recur (conj stack bracket) rest-brackets)
-        (let [prev-bracket (peek stack)]
-          (if (= bracket (matching-bracket prev-bracket))
-            (recur (pop stack) rest-brackets)
-            [:corrupted bracket])))
+         [char & rest-chars] s]
+    (if char
+      (if (open-bracket? char)
+        (recur (conj stack char) rest-chars)
+        (let [prev-char (peek stack)]
+          (if (= char (matching-char prev-char))
+            (recur (pop stack) rest-chars)
+            [:corrupted char])))
       (if (empty? stack)
         [:valid []]
-        [:incomplete (complete-chunk stack)]))))
+        [:incomplete stack]))))
 
 (defn process-all-chunks
+  "Applies process-chunk to the chunks collection."
   []
   (map process-chunk chunks))
 
@@ -55,11 +59,14 @@
 ; --------------------------
 ; problem 1
 
-(defn illegal-brackets
+(defn illegal-chars
+  "Returns a collection of the illegal chars, one for each corrupted chunk."
   []
-  (filter #(= :corrupted (first %)) (memoized-process-all-chunks)))
+  (->> (memoized-process-all-chunks)
+       (filter #(= :corrupted (first %)))
+       (map second)))
 
-(def illegal-bracket-vals
+(def illegal-char-val
   {\) 3
    \] 57
    \} 1197
@@ -69,35 +76,39 @@
 ; problem 2
 
 (defn incomplete-chunks
+  "Returns a collection the unclosed char sequences, one sequence for each
+  incomplete chunk."
   []
-  (filter #(= :incomplete (first %)) (memoized-process-all-chunks)))
+  (->> (memoized-process-all-chunks)
+       (filter #(= :incomplete (first %)))
+       (map second)))
 
-(def incomplete-bracket-vals
+(def incomplete-char-val
   {\) 1
    \] 2
    \} 3
    \> 4})
 
 (defn incomplete-chunk-val
-  [chunk]
-  (reduce (fn [result bracket]
-            (+ (incomplete-bracket-vals bracket) (* 5 result)))
-          0 chunk))
+  "Returns the score of an incomplete chunk."
+  [unclosed-chars]
+  (let [missing-chars (complete-chunk unclosed-chars)]
+    (reduce (fn [result char]
+              (+ (incomplete-char-val char) (* 5 result)))
+            0 missing-chars)))
 
 ; --------------------------
 ; results
 
 (defn day10-1
   []
-  (->> (illegal-brackets)
-       (map second)
-       (map illegal-bracket-vals)
+  (->> (illegal-chars)
+       (map illegal-char-val)
        (apply +)))
 
 (defn day10-2
   []
   (let [vals (->> (incomplete-chunks)
-                  (map second)
                   (map incomplete-chunk-val)
                   sort)]
     (nth vals (/ (dec (count vals)) 2))))
