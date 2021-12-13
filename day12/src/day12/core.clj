2970cebba3200c0260ea33623f71e10a1b521a36
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-13 09:31:36 +0200

    day12 problem 1: expand a path with the caves that can be visited from the last cave

--- day12/src/day12/core.clj
+++ day12/src/day12/core.clj
@@ -41,6 +41,31 @@
        (filter #(= (name %) (clojure.string/upper-case (name %))))
        set))
 
+; --------------------------
+; problem 1
+
+(defn next-caves1
+  "Returns the caves that can be visited when we are in the last cave
+  of the path (problem 1)."
+  [cave path]
+  (remove #(and (not (contains? large-caves %))
+                (contains? (set path) %))
+          (get cave-map cave)))
+
+(defn expand-path1
+  "Returns a vector of all paths that can be formed by appending
+  each allowed next cave when we are in the last cave of the path (vector),
+  to the end of the given path (problem 1).
+  If there are 0 next caves and we are not in the :end cave, an empty
+  vector is returned (path is incomplete)."
+  [path]
+  (if (= :end (last path))
+    [path]
+    (let [allowed-caves (next-caves1 (last path) path)]
+      (if (seq allowed-caves)
+        (reduce #(conj %1 (conj path %2)) [] allowed-caves)
+        []))))
+
 (defn -main
   []
   (println cave-map))
