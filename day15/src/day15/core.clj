566992cb7c3e3d13483be349d9744c803ff43baa
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-17 20:52:45 +0200

    day15: find the min total cost to each location

--- day15/src/day15/core.clj
+++ day15/src/day15/core.clj
@@ -1,5 +1,6 @@
 (ns day15.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.priority-map :refer [priority-map]]))
 
 ; --------------------------
 ; common
@@ -23,6 +24,36 @@
 (def rows (count risk-levels))
 (def columns (count (first risk-levels)))
 
+(defn process-adjacent
+  "Updates the queue location cost when given a known from-cost."
+  [queue visited loc from-cost x-or-y invalid-x-or-y]
+  (if (or (= x-or-y invalid-x-or-y) (contains? visited loc))
+    queue
+    (let [new-cost (+ from-cost (get-in risk-levels loc))
+          curr-cost (get queue loc)]
+      (if (or (not curr-cost) (< new-cost curr-cost))
+        (assoc queue loc new-cost)
+        queue))))
+
+(defn find-min-sum
+  "Returns a map of [location -> total risk cost to location]. Start location is top-left."
+  ([rows columns]
+   (let [queue (priority-map [0 0] 0)]
+     (find-min-sum queue {} rows columns)))
+  ([queue visited rows columns]
+   (loop [queue queue
+          visited visited]
+     (if-let [[[x y :as loc] from-cost] (first queue)]
+       (let [new-queue (-> queue
+                           (process-adjacent visited [(inc x) y] from-cost (inc x) rows)
+                           (process-adjacent visited [x (inc y)] from-cost (inc y) columns)
+                           (process-adjacent visited [x (dec y)] from-cost (dec y) -1)
+                           (process-adjacent visited [(dec x) y] from-cost (dec x) -1)
+                           (dissoc loc))
+             new-visited (assoc visited loc from-cost)]
+         (recur new-queue new-visited))
+       visited))))
+
 (defn -main
   []
-  (println risk-levels))
+  (println (find-min-sum rows columns)))
