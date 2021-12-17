2ce03aaea020c2d5e5a9d7a4000cee7107ef38b3
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-17 20:33:27 +0200

    day15: parse input

--- day15/src/day15/core.clj
+++ day15/src/day15/core.clj
@@ -1,7 +1,28 @@
 (ns day15.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn char->int
+  "Converts a digit char to the corresponding integer digit."
+  [c]
+  (- (int c) 48))
+
+(defn parse
+  "Parses the input string and returns a vector of vectors of integers.
+  Each vector corresponds to a line in the input file."
+  [s]
+  (->> s
+       clojure.string/split-lines
+       (mapv #(mapv char->int %))))
+
+(def risk-levels (parse (slurp input-file)))
+(def rows (count risk-levels))
+(def columns (count (first risk-levels)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println risk-levels))
