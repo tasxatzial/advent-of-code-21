b9b07155b4656229ea1600c7bde1e4e48eae954b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-23 22:42:10 +0200

    day21 problem 1: roll dice 3 times and calculate new score and position

--- day21/src/day21/core.clj
+++ day21/src/day21/core.clj
@@ -1,7 +1,38 @@
 (ns day21.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def player1-pos 8)
+(def player2-pos 10)
+
+(defn dice-sum1
+  "Returns the sum of the 3 dice roll results (problem 1)."
+  [turn]
+  (+ 6 (* 9 (dec turn))))
+
+(defn next-pos
+  "Returns the next position.
+  pos: current player position
+  sum: sum of the 3 roll results."
+  [pos sum]
+  (let [new-pos (rem (+ pos sum) 10)]
+    (if (zero? new-pos)
+      10
+      new-pos)))
+
+(defn move
+  "Returns a vector of two items. First item is the new score of the player,
+  second item is the new position of the player.
+  score: current player score
+  pos: current player position"
+  [turn score pos]
+  (let [dice-sum (dice-sum1 turn)
+        new-pos (next-pos pos dice-sum)
+        new-score (+ score new-pos)]
+    [new-score new-pos]))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (move 1 0 player1-pos)))
