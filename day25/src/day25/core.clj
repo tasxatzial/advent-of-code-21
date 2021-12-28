784512087a80d35563356db18170d65163421d55
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-28 23:01:57 +0200

    day25: parse input

--- day25/src/day25/core.clj
+++ day25/src/day25/core.clj
@@ -1,7 +1,39 @@
 (ns day25.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Parses the input and returns a vector of 3 items. First item is the
+  rows, second is the columns, third is a map of cucumber-location-> moving direction."
+  [s]
+  (let [split-lines (clojure.string/split-lines s)
+        rows (count split-lines)
+        columns (count (first split-lines))
+        locations (for [i (range rows)
+                        j (range columns)]
+                    [i j])
+        sea-cucumbers (flatten (map #(map identity %) split-lines))]
+    [rows columns (zipmap locations sea-cucumbers)]))
+
+(def parsed-input (parse (slurp input-file)))
+(def rows (first parsed-input))
+(def columns (second parsed-input))
+(def sea-cucumbers (get parsed-input 2))
+
+(defn collect-east-cucumbers
+  "Returns a set of the locations of the cucumbers that move east."
+  []
+  (into #{} (map first (filter #(= \> (second %)) sea-cucumbers))))
+
+(defn collect-south-cucumbers
+  "Returns a set of the locations of the cucumbers that move south."
+  []
+  (into #{} (map first (filter #(= \v (second %)) sea-cucumbers))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println sea-cucumbers))
