8ab562612cfde2c9a9047b35764fc0fdcc053a02
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-12-28 23:13:27 +0200

    day25: find next location of cucumbers

--- day25/src/day25/core.clj
+++ day25/src/day25/core.clj
@@ -34,6 +34,44 @@
   []
   (into #{} (map first (filter #(= \v (second %)) sea-cucumbers))))
 
+(defn next-east-loc
+  "Returns the location to the east of the given location [x y]."
+  [[x y]]
+  (let [next-y (inc y)]
+    (if (= next-y columns)
+      [x 0]
+      [x next-y])))
+
+(defn next-south-loc
+  "Returns the location to the south of the given location [x y]."
+  [[x y]]
+  (let [next-x (inc x)]
+    (if (= next-x rows)
+      [0 y]
+      [next-x y])))
+
+(defn move
+  "Returns a set of the new locations of the given moving-cucumbers.
+  fn_move: either next-east-loc or next-south-loc depending on whether
+  the moving-cucumbers set is moving to the east or to the south."
+  [moving-cucumbers other-cucumbers fn_move]
+  (reduce (fn [result loc]
+            (let [next-loc (fn_move loc)]
+              (if (or (contains? other-cucumbers next-loc)
+                      (contains? moving-cucumbers next-loc))
+                (conj result loc)
+                (conj result next-loc))))
+          #{} moving-cucumbers))
+
+(defn move-once
+  "Returns a vector of two items. First item is a set of the new locations of the
+  cucumbers moving east. Second item is a set of the new locations of the cucumbers
+  moving south."
+  [east-cucumbers south-cucumbers]
+  (let [new-east-cucumbers (move east-cucumbers south-cucumbers next-east-loc)
+        new-south-cucumbers (move south-cucumbers new-east-cucumbers next-south-loc)]
+    [new-east-cucumbers new-south-cucumbers]))
+
 (defn -main
   []
   (println sea-cucumbers))
