da1fd5757b02fd0aecbff7de1a53c60a0be8ca74
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-03 02:54:27 +0200

    day17: find the bounds for the x-vel and y-vel

--- day17/src/day17/core.clj
+++ day17/src/day17/core.clj
@@ -1,6 +1,20 @@
 (ns day17.core
   (:gen-class))
 
+; the solution below depends on the input
+
+; --------------------------
+; common
+
+(def target-xmin 288)
+(def target-xmax 330)
+(def target-ymax -50)
+(def target-ymin -96)
+(def min-vel-x (Math/round (Math/floor (/ (dec (Math/sqrt (inc (* 8 target-xmin)))) 2))))
+(def max-vel-x target-xmax)
+(def min-vel-y target-ymin)
+(def max-vel-y (- target-ymin))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
