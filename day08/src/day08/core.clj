e98a8afa316ad946fd57cd4c292cd72a3a36eaf8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-01-16 14:14:57 +0200

    day18 problem 1: refactoring

--- day08/src/day08/core.clj
+++ day08/src/day08/core.clj
@@ -22,40 +22,44 @@
 
 (def signals (parse (slurp input-file)))
 
-(def digit->segment-count
-  {0 6
-   1 2
-   2 5
-   3 5
-   4 4
-   5 5
-   6 6
-   7 3
-   8 7
-   9 6})
+(def segments->digit
+  {#{1 2 3 5 6 7} 0
+   #{3 6} 1
+   #{1 3 4 5 7} 2
+   #{1 3 4 6 7} 3
+   #{2 3 4 6} 4
+   #{1 2 4 6 7} 5
+   #{1 2 4 5 6 7} 6
+   #{1 3 6} 7
+   #{1 2 3 4 5 6 7} 8
+   #{1 2 3 4 6 7} 9})
 
 ; --------------------------
 ; problem 1
 
-(def output-values (map second signals))
-
-(defn count-digits-with-unique-segments
-  "Takes as input a collection of 4 digits and counts how many times 1 4 7 8 appear in total."
-  [output-value]
-  (let [unique-segments (set (map digit->segment-count [1 4 7 8]))]
-    (count (filter unique-segments (map count output-value)))))
-
-(defn total-digits-with-unique-segments
-  "Finds how many times digits 1 4 7 8 appear in the output values."
+(defn unique-segment-count
+  "Returns the segment counts that appear one time."
   []
-  (apply + (map count-digits-with-unique-segments output-values)))
+  (->> segments->digit
+       keys
+       (map count)
+       frequencies
+       (filter #(= 1 (second %)))
+       (map first)
+       set))
+
 
 ; --------------------------
 ; results
 
 (defn day08-1
   []
-  (total-digits-with-unique-segments))
+  (let [unique-segment-count (unique-segment-count)]
+    (->> (map second signals)
+         (map #(map (fn [x] (count x)) %))
+         (map #(filter unique-segment-count %))
+         (map count)
+         (apply +))))
 
 (defn -main
   []
